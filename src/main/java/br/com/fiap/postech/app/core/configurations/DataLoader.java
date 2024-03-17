1f076dad2db1394687b214987d7996351fa40aa1
João Gabriel <jgcamposneto@gmail.com>
2024-03-17 03:25:59 -0300

    Dados para inicialização

--- src/main/java/br/com/fiap/postech/app/core/configurations/DataLoader.java
+++ src/main/java/br/com/fiap/postech/app/core/configurations/DataLoader.java
@@ -1,4 +1,26 @@
 package br.com.fiap.postech.app.core.configurations;
 
-public class DataLoader {
+import br.com.fiap.postech.app.gestaoquarto.data.datasources.JpaPredioRepository;
+import br.com.fiap.postech.app.gestaoquarto.data.models.PredioModel;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    final JpaPredioRepository jpaPredioRepository;
+
+    public DataLoader(JpaPredioRepository jpaPredioRepository) {
+        this.jpaPredioRepository = jpaPredioRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        PredioModel predioModel1 = new PredioModel();
+        predioModel1.setLocalizacao("Centro");
+        predioModel1.setLocalizacao("Periferia");
+        predioModel1.setLocalizacao("Orla");
+        jpaPredioRepository.save(predioModel1);
+    }
 }
